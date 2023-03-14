58df87c098f77c7129955000c0eae21f6566f503
shuo <caihehuang@gmail.com>
2023-03-14 08:09:03 +0800

    set up type scale/custom fonts/colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "the-new-css-reset/css/reset.css"
+import './rootCss.scss'
+import './font.css'
 
 function App() {
     return (
         <div className="App">
-            <h1>test123</h1>
+            <h1>现在是离国庆假期还有天离12号发工资还有摸鱼时间专心摸鱼停止打工摸鱼重要的是心态开始外国摸鱼人里克·鲁宾链接周末单休周末双休清明节劳动节端午节国庆节离5号发工资还有离10号发工资还有12152530</h1>
         </div>
     );
 }
