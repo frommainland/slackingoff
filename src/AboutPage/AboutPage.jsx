d7d48bcfdc780ffaf6291b9806c3cffb9929e5f4
frommainland <caihehuang@gmail.com>
2023-03-16 15:38:26 +0800

    add footer to AboutPage / animation for section images

--- src/AboutPage/AboutPage.jsx
+++ src/AboutPage/AboutPage.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef } from 'react'
 import './AboutPage.scss'
 import { motion, useScroll, useMotionValueEvent } from 'framer-motion'
+import Footer from '../components/Footer'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
+import userEvent from '@testing-library/user-event'
 gsap.registerPlugin(ScrollTrigger)
+
 
 const ALink = ({ text, link }) => {
 	return (
@@ -14,23 +17,14 @@ const ALink = ({ text, link }) => {
 }
 
 const memeArr = [1, 2, 3, 4, 5, 6, 7]
-// const TopLeft = [
-// 	['80%', '0%'],
-// 	['100%', '40%'],
-// 	['180%', '20%'],
-// 	['240%', '140%'],
-// 	['120%', '210%'],
-// 	['95%', '290%'],
-// 	['120%', '400%'],
-// ]
 const TopLeft = [
 	['0%', '0%'],
-	['25%', '50%'],
-	['130%', '20%'],
-	['140%', '140%'],
-	['64%', '210%'],
-	['16%', '295%'],
-	['120%', '400%'],
+	['-40%', '50%'],
+	['32%', '20%'],
+	['-16%', '140%'],
+	['100%', '210%'],
+	['5%', '295%'],
+	['-48%', '400%'],
 ]
 
 const MemeImage = ({ constraintsRef, item, top, left }) => {
@@ -62,15 +56,32 @@ const AboutPage = () => {
 	// 	console.log('Page scroll: ', latest)
 	// })
 	const startImgRef = useRef(null)
+	// meme drag consraints
+
 	const constraintsRef = useRef(null)
+
+	const rubinRef = useRef(null)
+
 	useEffect(() => {
 		const el = startImgRef.current
+		const rubin = rubinRef.current
 		gsap.to(el, {
 			rotate: 5,
 			scale: 1.5,
 			y: -150,
 			scrollTrigger: {
 				trigger: el,
+				start: 'top, 80%',
+				end: 'top, 20%',
+				scrub: 1,
+			},
+		})
+		gsap.to(rubin, {
+			rotate: 0,
+			scale: 1.1,
+			y: -350,
+			scrollTrigger: {
+				trigger: rubin,
 				start: 'top, 80%',
 				end: 'top, 20%',
 				scrub: 1,
@@ -103,7 +114,7 @@ const AboutPage = () => {
 					className="start-image"
 				/>
 			</section>
-			<section className="content-wrap">
+			<section className="content-wrap meme-index">
 				<div className="title">
 					<span>02</span>
 					<p>外国摸鱼人</p>
@@ -132,7 +143,7 @@ const AboutPage = () => {
 					<span>03</span>
 					<p>里克·鲁宾</p>
 				</div>
-				<div className="content">
+				<div className="content rubin">
 					<p>网站最后的视频是Rick Rubin的一次采访。</p>
 					<ALink
 						text="Rick Rubin's stripped down approach to making music"
@@ -151,11 +162,73 @@ const AboutPage = () => {
 						但是这个视频无论从讽刺或是正向的角度来看，我都觉得适合放在这里。如果周围“有品位”的人都这样，那么摸鱼人在打工的时候就更要有这种自信的态度。
 					</p>
 					<img
+						ref={rubinRef}
 						src={require('../images/rick_rubin.jpg')}
 						alt="rick rubin portrait"
 					/>
 				</div>
 			</section>
+			<section className="content-wrap">
+				<div className="title">
+					<span>04</span>
+					<p>链接</p>
+				</div>
+				<div className="content link-list">
+					<div className="link-wrap">
+						<p>字体</p>
+						<div className="link-item-wrap">
+							<ALink
+								text="上首幻灵体"
+								link="https://www.cbsnews.com/news/rick-rubin-anderson-cooper-60-minutes-interview-2023-01-15/"
+							/>
+							<ALink
+								text="KEROZENE"
+								link="https://www.cbsnews.com/news/rick-rubin-anderson-cooper-60-minutes-interview-2023-01-15/"
+							/>
+						</div>
+					</div>
+					<div className="link-wrap">
+						<p>参考</p>
+						<div className="link-item-wrap">
+							<ALink
+								text="tympanus"
+								link="https://www.cbsnews.com/news/rick-rubin-anderson-cooper-60-minutes-interview-2023-01-15/"
+							/>
+						</div>
+					</div>
+					<div className="link-wrap">
+						<p>代码</p>
+						<div className="link-item-wrap">
+							<ALink
+								text="github"
+								link="https://www.cbsnews.com/news/rick-rubin-anderson-cooper-60-minutes-interview-2023-01-15/"
+							/>
+						</div>
+					</div>
+					<div className="link-wrap">
+						<p>相关项目</p>
+						<div className="link-item-wrap">
+							<ALink
+								text="花与诗花和人"
+								link="https://www.cbsnews.com/news/rick-rubin-anderson-cooper-60-minutes-interview-2023-01-15/"
+							/>
+							<ALink
+								text="互联网词语学习"
+								link="https://www.cbsnews.com/news/rick-rubin-anderson-cooper-60-minutes-interview-2023-01-15/"
+							/>
+							<ALink
+								text="天王盖地虎，宝塔镇河妖"
+								link="https://www.cbsnews.com/news/rick-rubin-anderson-cooper-60-minutes-interview-2023-01-15/"
+							/>
+							<ALink
+								text="其他"
+								link="https://www.cbsnews.com/news/rick-rubin-anderson-cooper-60-minutes-interview-2023-01-15/"
+							/>
+						</div>
+					</div>
+				</div>
+			</section>
+			<Footer />
 		</main>
 	)
 }
